507778ada722ccc0e5f3ba048f8b66ebee0d788b
David Morales <davidmg744@gmail.com>
2024-11-03 20:19:06 -0600

    Se actualizo el mensaje en HolaMundo.java

--- codigo/HolaMundo.java
+++ codigo/HolaMundo.java
@@ -2,6 +2,6 @@ package codigo;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola Git");
     }
 }
